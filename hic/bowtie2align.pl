7bc0c20d8f1bc7d25a000254395522097cc090b6
Vittore F. Scolari <vittore.scolari@upmc.fr>
2013-11-13 18:08:44 +0100

    hic: Automatically select the number of cores for aligning

--- hic/bowtie2align.pl
+++ hic/bowtie2align.pl
@@ -4,7 +4,9 @@ sub bowtie2align
 {
     print "aligning $_[0] to $_[1]\n";
 #    my $settings="--score-min L,0.6,0.2 --very-sensitive -p 16 -q";
-    my $settings="--very-sensitive -p 14 --reorder -q";
+# use 1 less core then the number of cores installed
+    my $cores=`grep "^cpu cores" /proc/cpuinfo | sort -u | cut -f 2 -d :` * `grep "^physical id" /proc/cpuinfo | sort -u | wc -l` - 1;
+    my $settings="--very-sensitive -p $cores --reorder -q";
     my $ref=$_[1];
     my $comando="bowtie2 $settings -x $ref -U $_[0]";
 
